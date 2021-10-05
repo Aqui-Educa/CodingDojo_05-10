9ad732b04e05707ed3394d0b6d3d66d6d9cc05a6
HuanCarlos13 <201810297@acad.unijuazeiro.edu.br>
2021-10-05 20:33:44 -0300

    agora vai

--- Carro.py
+++ Carro.py
@@ -1,14 +1,43 @@
 class Carro:
-    def __init__(self, cor, marca, modelo, status):
+    def __init__(self, cor, marca, modelo, status,valor):
         self._cor = cor
         self._marca = marca
         self._modelo = modelo
         self._status = status
+        self._valor = valor
     
     def get_cor(self):
         return self._cor
+
     def set_cor(self, cor):
-        pass
+        self._cor = cor
+
+    def get_marca(self):
+        return self._marca
+
+    def set_marca(self, marca):
+        self._marca = marca
+
+    def get_modelo(self):
+        return self._modelo
+
+    def set_modelo(self, modelo):
+        self._modelo = modelo
+
+    def get_status(self):
+        return self._status
+
+    def set_status(self, status):
+        self._status = status
+
+    def get_valor(self):
+        return self._valor
+
+    def set_valor(self, valor):
+        self._valor = valor
+
+    
+
     
     def ligar(self):
         if(self._status == True):
